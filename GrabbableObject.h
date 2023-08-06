6bebabbdce4a69e6d19626d4b0d14cf2df575ac8
Deyan Bora Çetin <97949641+DeyanBora@users.noreply.github.com>
2023-08-06 14:48:14 +0300

    GrabbableObject.h

---
+++ GrabbableObject.h
@@ -0,0 +1,26 @@
+	#pragma once
+ 
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GrabbableObject.generated.h"
+ 
+UCLASS()
+class GRABOBJECT_API AGrabbableObject : public AActor
+{
+	GENERATED_BODY()
+	 
+public:
+	//Object Mesh
+	UPROPERTY(EditAnywhere, Category = "Object Properties")
+	UStaticMeshComponent* StaticMesh;
+ 
+public:	
+
+	AGrabbableObject();
+ 
+public:	
+
+	virtual void Tick(float DeltaTime) override;
+
+  virtual void BeginPlay() override;
+};
